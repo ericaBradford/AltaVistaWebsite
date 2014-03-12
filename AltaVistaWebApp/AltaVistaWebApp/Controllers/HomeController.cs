683f9549cbb4c418d7d07fd626599836996a9dda
Zach O'Sullivan <zachobennett@gmail.com>
2014-03-12 10:26:52 -0600

    Controller Set up, home page added, and style changes

---
+++ AltaVistaWebApp/AltaVistaWebApp/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AltaVistaWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult Faculty()
+        {
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult Announcements()
+        {
+            return View();
+        }
+
+        public ActionResult ProfilePayments()
+        {
+            return View();
+        }
+
+        public ActionResult ProfileGrades()
+        {
+            return View();
+        }
+
+        public ActionResult ProfileAssignments()
+        {
+            return View();
+        }
+
+        public ActionResult SchoolLife()
+        {
+            return View();
+        }
+
+
+
+    }
+}
